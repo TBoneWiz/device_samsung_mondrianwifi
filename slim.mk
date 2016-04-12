aa411ffe99889787a474bec4a6dba681c8fb9150
Josue Rivera <prbassplayer@gmail.com>
2016-04-12 04:55:00 +0200

    Mondrianwifi: Fix mk calls
    Change-Id: I33cf622c8930dd5a2ed5de29d8c7939424601743
    Signed-off-by: Josue Rivera <prbassplayer@gmail.com>

--- slim.mk
+++ slim.mk
@@ -3,7 +3,7 @@ TARGET_SCREEN_HEIGHT := 2560
 TARGET_SCREEN_WIDTH := 1600
 
 # Inherit some common Slim stuff.
-$(call inherit-product, vendor/slim/config/common_full_tablet_wifionly.mk)
+$(call inherit-product, vendor/slim/config/common.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/mondrianwifi/full_mondrianwifi.mk)
